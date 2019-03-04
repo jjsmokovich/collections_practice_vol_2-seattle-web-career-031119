cbbbf26ef520549b9ee33c84752aa7454a20704c
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-04 17:06:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,6 +28,6 @@ end
 
 def merge_data(keys, data)
   merged = []
-  keys.each {|i| data.first.map {|k,v| if i.values[0] == k then merged << i.merge(v) end}}
+  keys.each {|i| data.first.map {|a,t| if i.values[0] == k then merged << i.merge(v) end}}
   merged
 end
