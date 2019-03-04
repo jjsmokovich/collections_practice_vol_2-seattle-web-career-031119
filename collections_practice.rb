e8975731b435fb92080e2a552ccb57f6eea0361e
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-04 16:21:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|element| element.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select {|element| element.include?("a")}
+end
+
+def first_wa(array)
+   array.find do |element| element[0,2] == "wa"
+  end
+end 
+
+def remove_non_strings(array)
+  array.delete_if {|element| element.class != String}
+end
+
+def count_elements(arr)
+  arr.uniq.map { |x| [x, arr.count(x)] }.
+end
+
+
+
